0b053262eee2ea371dff3fbd3146b71645965c62
Koby Bibas <kobybibas@gmail.com>
2018-11-12 14:10:11 +0200

    Update results summary

--- src/analyze_utilities.py
+++ src/analyze_utilities.py
@@ -139,22 +139,23 @@ def get_NML_log_loss_of_the_series_from_dict(results_dict, trainset_size=50000):
     return loss_NML_list, normalization_factor_list, acc_NML, is_correct_list, test_sample_idx_list
 
 
-def calculate_top_k_acc(results_dict, top_k):
+def calculate_top_k_acc(results_dict, top_k, prob_thresh=0.0):
     is_correct_nml_list = []
     is_correct_erm_list = []
     test_sample_idx_list = []
     for keys in results_dict:
         sample_dict = results_dict[keys]
         prob, true_label, predicted_label, prob_org = extract_probabilities_list(sample_dict)
+        normalized_prob, _ = execute_normalize_prob(prob)
 
-        top_k_labels = np.argsort(prob)[-top_k:][::-1]
-        if true_label in top_k_labels:
+        top_k_labels = np.argsort(normalized_prob)[-top_k:][::-1].astype(int)
+        if true_label in top_k_labels and normalized_prob[true_label] > prob_thresh:
             is_correct_nml_list.append(True)
         else:
             is_correct_nml_list.append(False)
 
-        top_k_labels = np.argsort(prob_org)[-top_k:][::-1]
-        if true_label in top_k_labels:
+        top_k_labels = np.argsort(prob_org)[-top_k:][::-1].astype(int)
+        if true_label in top_k_labels and prob_org[true_label] > prob_thresh:
             is_correct_erm_list.append(True)
         else:
             is_correct_erm_list.append(False)
@@ -162,6 +163,7 @@ def calculate_top_k_acc(results_dict, top_k):
         test_sample_idx_list.append(keys)
     acc_top_k_nml = np.sum(is_correct_nml_list) / len(is_correct_nml_list)
     acc_top_k_erm = np.sum(is_correct_erm_list) / len(is_correct_erm_list)
+
     return acc_top_k_nml, acc_top_k_erm
 
 
@@ -210,30 +212,34 @@ def get_training_loss_list_from_dict(results_dict):
 def load_dict_from_file_list(files):
     result_dict = {}
     for file in files:
-        with open(file) as data_file:
-            result_dict.update(json.load(data_file))
+        with open(file) as f:
+            result_dict.update(json.load(f))
     return result_dict
 
 
 def load_results_to_df(files):
     results_dict = load_dict_from_file_list(files)
 
-    log_nml_list, normalization_factor_list, acc, is_correct_nml_list, test_sample_idx = get_NML_log_loss_from_dict(
+    log_nml_list, normalization_factor, acc, is_correct_nml_list, test_sample_idx = get_NML_log_loss_from_dict(
         results_dict)
     loss_erm_list, acc_erm, is_correct_erm_list = get_ERM_log_loss_from_dict(results_dict)
-    loss_jinni_list, acc_Jinni, is_correct_jinni_list = get_jinni_log_loss_from_dict(results_dict)
+    loss_jinni_list, acc_jinni, is_correct_jinni_list = get_jinni_log_loss_from_dict(results_dict)
 
     test_sample_idx = np.array(test_sample_idx).astype(int)
 
     dict_for_df = {'nml_loss': pd.Series(log_nml_list, index=test_sample_idx),
                    'erm_loss': pd.Series(loss_erm_list, index=test_sample_idx),
                    'jinni_loss': pd.Series(loss_jinni_list, index=test_sample_idx),
-                   'log10_norm_factor': pd.Series(np.log10(normalization_factor_list), index=test_sample_idx),
+                   'log10_norm_factor': pd.Series(np.log10(normalization_factor), index=test_sample_idx),
                    'nml_is_correct': pd.Series(is_correct_nml_list, index=test_sample_idx),
                    'erm_is_correct': pd.Series(is_correct_erm_list, index=test_sample_idx),
                    'jinni_is_correct': pd.Series(is_correct_jinni_list, index=test_sample_idx)}
     result_df = pd.DataFrame(dict_for_df)
 
+    return result_df
+
+
+def calc_statistic_from_df(result_df):
     # calc mean and std
     mean_loss_jinni, std_loss_jinni = result_df['jinni_loss'].mean(), result_df['jinni_loss'].std()
     mean_loss_nml, std_loss_nml = result_df['nml_loss'].mean(), result_df['nml_loss'].std()
@@ -245,16 +251,12 @@ def load_results_to_df(files):
     acc_erm = result_df['erm_is_correct'][result_df['erm_is_correct'] == True].count() / count
     acc_nml = result_df['nml_is_correct'][result_df['nml_is_correct'] == True].count() / count
 
-    statistics = {'jinni': pd.Series([acc_jinni, mean_loss_jinni, std_loss_jinni],
-                                     index=['acc', 'mean loss', 'std loss']),
-                  'nml': pd.Series([acc_nml, mean_loss_nml, std_loss_nml],
-                                   index=['acc', 'mean loss', 'std loss']),
-                  'erm': pd.Series([acc_erm, mean_loss_erm, std_loss_erm],
-                                   index=['acc', 'mean loss', 'std loss'])}
+    stat = {'jinni': pd.Series([acc_jinni, mean_loss_jinni, std_loss_jinni], index=['acc', 'mean loss', 'std loss']),
+            'nml': pd.Series([acc_nml, mean_loss_nml, std_loss_nml], index=['acc', 'mean loss', 'std loss']),
+            'erm': pd.Series([acc_erm, mean_loss_erm, std_loss_erm], index=['acc', 'mean loss', 'std loss'])}
+    statistics_df = pd.DataFrame(stat)
 
-    statistics_df = pd.DataFrame(statistics)
-
-    return result_df, statistics_df
+    return statistics_df
 
 
 if __name__ == "__main__":
